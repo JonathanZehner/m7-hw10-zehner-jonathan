538445f0580b35b518846015227a8c5bb84e17de
Jonathan Zehner <jonathan.zehner@ufl.edu>
2021-12-09 13:37:55 -0500

    Attempt debug cookie retrieval

--- index.js
+++ index.js
@@ -6,22 +6,34 @@ var textarea = document.querySelector('textarea')
 
 // Retrieve name and note content from cookies and localstorage
 // Then apply them to elements on the page
+var nameEl = nameSpan.textContent;
+document.cookie = "name=" + nameEl;
+console.log(nameEl);
+cookieStore.get('span')
+  .then(function(cookieObj) {
+    console.log(cookieObj);
+    if (cookieObj) {
+      nameEl = cookieObj.value;
+    }
+  })
+  nameSpan.textContent = nameEl;
+
 
 formEl.onsubmit = function(e) {
   // prevents form submission
   e.preventDefault()
   // save name element's content to cookies
-  var nameEl = nameSpan.textContent;
-  document.cookie = "name=" + nameEl;
-  console.log(nameEl);
+  var userName = nameSpan.textContent;
+  document.cookie = "name=" + userName;
+  console.log(userName);
   cookieStore.get('span')
     .then(function(cookieObj) {
       console.log(cookieObj);
       if (cookieObj) {
-        nameEl = cookieObj.value;
+        userName = cookieObj.value;
       }
     })
-    nameSpan.textContent = nameEl;
+    nameSpan.textContent = userName;
 
   // save textarea's content to localstorage
   console.log('submitted')
